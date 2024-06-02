68d21019e4c928513894a700476357744c7b8c16
suryaprasad3 <suryaprasad.3@gmail.com>
2024-06-02 20:08:23 +0530

    project template added

---
+++ template.py
@@ -0,0 +1,32 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+# logging.info("Hello Everyone, welcome to this course.")
+
+list_of_files = [
+    'src/__init__.py',
+    'src/helper.py',
+    'src/prompt.py',
+    '.env',
+    'requirements.txt',
+    'setup.py',
+    'research/trails.ipynb',
+    'app.py'
+]
+
+# always make sure your code runs in windows, linux and mac
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    # print(filedir,"###" ,filename)
+    if filedir != "":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"Creating directory {filedir} for files {filename}")
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)== 0):
+        with open(filepath, "w") as f:
+            pass
+        logging.info(f"Creating empty file : {filepath}")
+    else:
+        logging.info(f"File {filepath} already exists")
